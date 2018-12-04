4e6042ca0ea94bafcb32c3ad88e326a5da19a043
Ronye do Lago <ronye.rocha@gmail.com>
2018-12-04 12:48:08 -0200

    ResultMap

---
+++ mhetrika.Infrastructure/Data/Mappings/ResultMap.cs
@@ -0,0 +1,17 @@
+﻿using mhetrika.core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace mhetrika.Infrastructure.Data.Mappings
+{
+    public class ResultMap : IEntityTypeConfiguration<Result>
+    {
+        public void Configure(EntityTypeBuilder<Result> builder)
+        {
+            builder.ToTable("Results");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Description)
+                .HasColumnType("varchar(8)");
+        }
+    }
+}

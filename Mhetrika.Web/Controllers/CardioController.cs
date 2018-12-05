5859c673f2d8f32fe0efcd6ef804375a5a45cb61
Ronye do Lago <ronye.rocha@gmail.com>
2018-12-05 18:46:10 -0200

    single

---
+++ Mhetrika.Web/Controllers/CardioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mhetrika.Web.Controllers
+{
+    public class CardioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

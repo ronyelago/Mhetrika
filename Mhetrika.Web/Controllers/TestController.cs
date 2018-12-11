aaf3ca1f28b84129c6292c17ba37d63f1ecb2327
Ronye <ronye.rocha@gmail.com>
2018-12-11 14:41:19 -0200

    corrections

--- Mhetrika.Web/Controllers/TestController.cs
+++ Mhetrika.Web/Controllers/TestController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Mhetrika.Web.Controllers
 {
     public class TestController : Controller
     {
-        public IActionResult ChoseTest()
+        public IActionResult ChooseTest()
         {
             return View();
         }

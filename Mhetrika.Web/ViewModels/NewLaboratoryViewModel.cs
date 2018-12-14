faca9c2f6451fe73fbf479f3e6f80c0d0bc2dce9
Ronye <ronye.rocha@gmail.com>
2018-12-14 15:48:23 -0200

    new lab view model

---
+++ Mhetrika.Web/ViewModels/NewLaboratoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Mhetrika.Web.ViewModels
+{
+    public class NewLaboratoryViewModel
+    {
+        public string Name { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+        public string Cep { get; set; }
+        public int Number { get; set; }
+        public string Street { get; set; }
+        public string Neighbourhood { get; set; }
+        public string City { get; set; }
+        public string Uf { get; set; }
+    }
+}

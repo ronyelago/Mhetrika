bd5ff0d2aa5513b1c1177bdf1b70d6bce5f3d05b
Ronye <ronye.rocha@gmail.com>
2018-12-17 13:03:26 -0200

    laboratory list view model

---
+++ Mhetrika.Web/ViewModels/LaboratoryListViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mhetrika.Web.ViewModels
+{
+    public class LaboratoryListViewModel
+    {
+        public string Name { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+    }
+}

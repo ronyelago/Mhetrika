698fbd8ce98880cee8d42f9221d9be93468d98ee
Ronye <ronye.rocha@gmail.com>
2018-12-17 13:14:42 -0200

    ids

--- Mhetrika.Web/ViewModels/LaboratoryListViewModel.cs
+++ Mhetrika.Web/ViewModels/LaboratoryListViewModel.cs
@@ -2,6 +2,7 @@ namespace Mhetrika.Web.ViewModels
 {
     public class LaboratoryListViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Cnpj { get; set; }
         public string Email { get; set; }

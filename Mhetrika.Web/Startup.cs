e0dd6738cc71628ee6409cf590f1901291ae55c7
Ronye <ronye.rocha@gmail.com>
2018-12-18 15:12:52 -0200

    home

--- Mhetrika.Web/Startup.cs
+++ Mhetrika.Web/Startup.cs
@@ -69,7 +69,7 @@ namespace Mhetrika.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Test}/{action=ChooseTest}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
